76002afa3427f940939a692c4c8ea7679b48af52
tobereborn <jinweizhen008@gmail.com>
2016-08-03 00:36:50 +0800

    Update shrotcuts and add git routines

---
+++ routines/git_common.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Common git commands,refering to http://marklodato.github.io/visual-git-guide/index-en.html
+# 
+
+echo "list all configs"
+git config --list
+
+echo "check remote branches"
+git remote  -v  
+
+
+
