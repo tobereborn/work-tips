62fa0943b7ca4e894ba580cf3b5259268f22eb79
(none) <(none)>
2016-08-23 16:09:42 +0800

    Add branch commands to git common shell

--- routines/git_common.sh
+++ routines/git_common.sh
@@ -3,11 +3,25 @@
 # Common git commands,refering to http://marklodato.github.io/visual-git-guide/index-en.html
 # 
 
+set -u
+set -e
+
 echo "list all configs"
 git config --list
 
 echo "check remote branches"
 git remote  -v  
 
+echo "check local branches"
+git branch -a
+
+echo "delete local branch that has merged to current branch"
+git branch -d "$branch"
 
 
+echo "delete remote branch"
+git push origin --delete <branchName>
+
+echo "purge local remote branch refers"
+git fetch -p
+git remote prune origin
