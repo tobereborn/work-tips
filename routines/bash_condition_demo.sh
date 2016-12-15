6445586b4bdcb3964ad37f1a392196faa8d10268
tobereborn <jinweizhen008@gmail.com>
2016-12-15 09:37:50 +0800

    Create bash_condition_demo.sh

---
+++ routines/bash_condition_demo.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+folder="/var/www/"
+file="/var/www/log"
+
+# execution premission
+if [ ! -x "$folder"]; then
+  mkdir "$folder"
+fi
+
+# dir existing
+if [ ! -d "$folder"]; then
+  mkdir "$folder"
+fi
+
+# file existing
+if [ ! -f "$file" ]; then
+  touch "$file"
+fi
+
+# empty var
+if [ ! -n "$var" ]; then
+  echo "$var is empty"
+  exit 0
+fi
+
+# equal string
+if [ "$var1" = "$var2" ]; then
+  echo '$var1 eq $var2'
+else
+  echo '$var1 not eq $var2'
+fi
